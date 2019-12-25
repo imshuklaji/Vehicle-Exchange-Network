ababc8de41a9ddf00dd51ed03834ce230e9d3c91
imshuklaji <shubshukla25@gmail.com>
2019-12-26 00:47:53 +0530

    Chaincode Skeletion Created

--- chaincode/contract.js
+++ chaincode/contract.js
@@ -11,9 +11,56 @@ class VexnetContract extends Contract {
 		global.utils = new utilsclass();
 
         //These Global variables are used to store the organisation names of the organisations participating in the network.
-		global.usersOrg = 'users.vehicle-exchange-network.com';
-		global.carOrg = 'carcompany.vehicle-exchange-network.com'
+		global.ind1Org = 'ind1.vehicle-exchange-network.com';
+		global.ind2Org = 'ind2.vehicle-exchange-network.com';
+		global.ind3Org = 'ind3.vehicle-exchange-network.com';
+		global.carOrg = 'carcompany.vehicle-exchange-network.com';
 	}
 
+
+	// Function to check for the intitiator of the transaction 
+	validateInitiator(ctx, initiator)
+	{
+		const initiatorID = ctx.clientIdentity.getX509Certificate();
+		console.log(initiator); 
+		if(initiatorID.issuer.organizationName.trim() !== initiator)
+		{
+				throw new Error('Not authorized to initiate the transaction: ' + initiatorID.issuer.organizationName + ' not authorised to initiate this transaction');
+		}
+	}
+
+
+	
+	/* ****** All custom functions are defined below ***** */
+	
+	// This is a basic user defined function used at the time of instantiating the smart contract
+	// to print the success message on console
+	async instantiate(ctx) {
+		console.log('Vexnet Smart Contract Instantiated');
+	}
+
+
+	/* Below set of the functions are for the Car Company ****** */
+
+	
+	// Register the new car on the network
+
+	 async function addCar(ctx,carSerialNo,carModelName,mfgDate,companyCRN) {
+
+
+	 }
+
+
+	 /* Function to transfer the ownership from Car company to individual */
+	 async function changeOwner(){
+
+	 }
+
+
+	 /* Function to view the owner of the Car */
+
+	 async function viewCarOwner() {
+
+	 }
 
 }
